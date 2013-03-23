40d59994d393c061f785b56279fc0471ea4f545e
jerome-genba <jerome@avoustin.com>
2013-03-24 00:52:30 +0100

    autres essais #2

--- testasserts.js
+++ testasserts.js
@@ -5,3 +5,11 @@
  * Time: 11:52 PM
  * To change this template use File | Settings | File Templates.
  */
+var casper = require('casper').create();
+
+casper.start('http://casperjs.org/index.html', function() {
+    this.test.assertExists('#about', 'ça marche !');
+});
+
+
+
